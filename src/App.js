f7fb32ff2b4fcc2d06724a22ae8974339c749c77
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-04-06 19:17:04 -0400

    form functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import './App.css';
 
-import React from "react";
+import React, { useState } from "react";
 import { Route, Link, Switch} from 'react-router-dom';
+import axios from 'axios';
 
 import Home from './components/Home';
 import PizzaForm from './components/PizzaForm';
 
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  pepperoni: false,
+  onions: false,
+  bpeppers: false,
+  mushrooms: false,
+  special: ''
+}
+
+const initialFormErrors = {
+  name: '',
+  size: '',
+}
+
+const initialOrders = [];
+//add variable for initial disabled
+
+
 const App = () => {
+
+  const [orders, setOrders] = useState(initialOrders)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  //add a state for disabled submit button
+
+  //create the validation function here
+
+  const inputChange = (name, value) => {
+    //run validation function here
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      pepperoni: formValues.pepperoni,
+      onions: formValues.onions,
+      bpeppers: formValues.bpeppers,
+      mushrooms: formValues.mushrooms,
+      special: formValues.special,
+    }
+
+    //axios.post here
+
+
+  }
+
   return (
     <div className='container'>
       <nav>
@@ -19,7 +72,13 @@ const App = () => {
 
       <Switch>
         <Route path="/pizza">
-          <PizzaForm />
+          <PizzaForm 
+              values={formValues}
+              change={inputChange}
+              submit={formSubmit}
+              //disabled={disabled}
+              errors={formErrors}
+          />
         </Route>
         <Route path="/">
           <Home />
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -2,51 +2,64 @@ import React from 'react'
 
 
 export default function PizzaForm(props) {
-
+    const {
+        values,
+        submit,
+        change,
+        //disabled,
+        errors,
+    } = props;
 
     const onChange = evt => {
-    
-}
+        const { type, name, value, checked } = evt.target
+        const valueToUse = type === "checkbox" ? checked : value
+        change(name, valueToUse);
+    }
 
 
-const onSubmit = evt => {
-    evt.preventDefault();
-
-  }
-
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+    }
 
 
     return (
         <form id='pizza-form' onSubmit={onSubmit}>
+
+            <div className='errors'>
+                <p>{errors.name}</p>
+                <p>{errors.size}</p>
+            </div>
+            
             <div className='form-inputs'>
-                {/* ////////// NAME TEXT INPUT ////////// */}
+                {/* ----- NAME TEXT INPUT ----- */}
                 <label>Name
                     <input
                         id="name-input"
                         type="text"
                         name="name"
-                        value=""
+                        value={values.name}
                         placeholder="Full Name"
                         minLength="2"
                         onChange={onChange}
                     />
                 </label>
-                {/* ////////// SIZE DROPDOWN ////////// */}
+                {/* ----- SIZE DROPDOWN ----- */}
                 <label>Size
-                    <select id="size-dropdown" value="" name="size" onChange={onChange}>
+                    <select id="size-dropdown" value={values.size} name="size" onChange={onChange}>
                         <option value="">- Select a Size -</option>
                         <option value="Small">Small</option>
                         <option value="Medium">Medium</option>
                         <option value="Large">Large</option>
                     </select>
                 </label>
-                {/* ////////// TOPPINGS CHECKLIST ////////// */}
+                {/* ----- TOPPINGS CHECKLIST ----- */}
                 <label>Pepperoni
                     <input
                         type="checkbox"
                         name="pepperoni"
                         onChange={onChange}
-                        checked=''
+                        checked={values.pepperoni}
                     />
                 </label>
                 <label>Onions
@@ -54,7 +67,7 @@ const onSubmit = evt => {
                         type="checkbox"
                         name="onions"
                         onChange={onChange}
-                        checked=''
+                        checked={values.onions}
                     />
                 </label>
                 <label>Banana Peppers
@@ -62,7 +75,7 @@ const onSubmit = evt => {
                         type="checkbox"
                         name="bpeppers"
                         onChange={onChange}
-                        checked=''
+                        checked={values.bpeppers}
                     />
                 </label>
                 <label>Mushrooms
@@ -70,21 +83,25 @@ const onSubmit = evt => {
                         type="checkbox"
                         name="mushrooms"
                         onChange={onChange}
-                        checked=''
+                        checked={values.mushrooms}
                     />
                 </label>
-                {/* ////////// SPECIAL INSTRUCTION TEXT INPUT ////////// */}
+                {/* ----- SPECIAL INSTRUCTION TEXT INPUT ----- */}
                 <label>Special Instructions
                     <input
                         id="special-text"
                         type="text"
-                        name="special-text"
-                        value=""
+                        name="special"
+                        value={values.special}
                         placeholder=""
                         maxLength="200"
                         onChange={onChange}
                     />
                 </label>
+            </div>
+            {/* ----- SUBMIT BUTTON ----- */}
+            <div className='form-submit'>
+                <button id='order-button'>Place Order</button>
             </div>
         </form>
         
